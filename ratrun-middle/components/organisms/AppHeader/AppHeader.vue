ed3d82208e2f2aa5fbdfb146982fa41d6fe12702
maeda kaoru <maedakaoru.0615@gmail.com>
2023-04-02 20:46:23 +0900

    [WIP]headerでdisplay:blockがデフォルト設定になっている

--- ratrun-middle/components/organisms/AppHeader/AppHeader.vue
+++ ratrun-middle/components/organisms/AppHeader/AppHeader.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>headerのコンポーネント</div>
+  <header>
+    <div class="haeder-title">
+      <p>Philosophy</p>
+      <p>理念</p>
+    </div>
+    <div class="menu">
+      <a href="#">ホーム</a>
+      <a href="#">私達について</a>
+      <a href="#">ホーム</a>
+      <a href="#">ホーム</a>
+      <a href="#">ホーム</a>
+      <a href="#">ホーム</a>
+      <a href="#">ホーム</a>
+      <a href="#">ホーム</a>
+    </div>
+  </header>
 </template>
+
 <script>
 export default {};
 </script>
+
 <style scoped>
+template{
+  padding: 0 0px;
+}
+
+.header-title {
+  font-family: DNPShueiShogoMinStd-Hv;
+  padding: 30 0px;
+  display: initial;
+
+}
 </style>
