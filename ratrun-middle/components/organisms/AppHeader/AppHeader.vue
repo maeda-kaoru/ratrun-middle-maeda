1a9760dfdee61d4f12513714d2c6d63b3f9b947f
maeda kaoru <maedakaoru.0615@gmail.com>
2023-04-05 19:10:29 +0900

    4/5 header作成途中

--- ratrun-middle/components/organisms/AppHeader/AppHeader.vue
+++ ratrun-middle/components/organisms/AppHeader/AppHeader.vue
@@ -1,18 +1,33 @@
 <template>
   <header>
     <div class="haeder-title">
-      <p>Philosophy</p>
-      <p>理念</p>
+      <p>Philosophy 理念</p>
+      <!-- <p>Philosophy</p>
+      <p>理念</p> -->
     </div>
-    <div class="menu">
-      <a href="#">ホーム</a>
-      <a href="#">私達について</a>
-      <a href="#">ホーム</a>
-      <a href="#">ホーム</a>
-      <a href="#">ホーム</a>
-      <a href="#">ホーム</a>
-      <a href="#">ホーム</a>
-      <a href="#">ホーム</a>
+    <div class="header-main">
+      <div class="header-img">
+        <img src="~/assets/img/グループ 120.png">
+        <img src="/Users/maedakaoru/Desktop/中級/ratrun-middle/assets/img/グループ 120.png">
+      </div>
+      <div class="menu">
+        <a href="#">ホーム</a>
+        <a href="#">私達について</a>
+        <a href="#">選ばれる理由</a>
+        <a href="#">ワークフロー</a>
+        <a href="#">FAQ</a>
+        <a href="#">事例紹介</a>
+        <a href="#">会社概要</a>
+        <a href="#">お問合せ・お見積</a>
+      </div>
+      <div class="header-right">
+        <p class="header-right-title">理念の文章が入ります</p>
+        <p>
+          母体が内装解体専門会社であるからこそ、一般的な施工管理よりも解体費用を抑える抑えることができます。つまり、
+          解体費用を抑えることで、余力のある資金が生まれ、使い道を広げることができるということです。
+          <br />選択のできる予算の使途も合わせてご提案します。
+        </p>
+      </div>
     </div>
   </header>
 </template>
@@ -22,14 +37,37 @@ export default {};
 </script>
 
 <style scoped>
-template{
+header {
   padding: 0 0px;
+  margin-bottom: 80px;
 }
 
-.header-title {
+.haeder-title {
   font-family: DNPShueiShogoMinStd-Hv;
   padding: 30 0px;
-  display: initial;
+  /* font-size: 84px; */
+}
+.header-main {
+  width: 100%;
+}
+.header-img {
+  background-image: url("~@/assets/img/グループ 120.png");
+}
+.menu a {
+  padding: 20px;
+}
+.header-right {
+  width: 50%;
+  padding-right: 50px;
+  margin-left: auto;
+}
+.header-right-title {
+  font-size: 70px;
+}
 
+@media screen and (max-width: 1080px) {
+  .header-right {
+    width: 80%;
+  }
 }
 </style>
