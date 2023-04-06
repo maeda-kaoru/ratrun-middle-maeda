99e7a31d80fc8040d69688a0b5b0f5e20e4650a7
maeda kaoru <maedakaoru.0615@gmail.com>
2023-04-06 17:24:48 +0900

    header作成　4/6

--- ratrun-middle/components/organisms/AppHeader/AppHeader.vue
+++ ratrun-middle/components/organisms/AppHeader/AppHeader.vue
@@ -1,60 +1,48 @@
 <template>
   <header>
-    <div class="haeder-title">
-      <p>Philosophy 理念</p>
-      <!-- <p>Philosophy</p>
-      <p>理念</p> -->
+    <div class="menu">
+      <nuxt-link v-for="(link, key) in links" :key="key" :to="link.to">{{
+        link.text
+      }}</nuxt-link>
     </div>
-    <div class="header-main">
-      <div class="header-img">
-        <img src="~/assets/img/グループ 120.png">
-        <img src="/Users/maedakaoru/Desktop/中級/ratrun-middle/assets/img/グループ 120.png">
-      </div>
-      <div class="menu">
-        <a href="#">ホーム</a>
-        <a href="#">私達について</a>
-        <a href="#">選ばれる理由</a>
-        <a href="#">ワークフロー</a>
-        <a href="#">FAQ</a>
-        <a href="#">事例紹介</a>
-        <a href="#">会社概要</a>
-        <a href="#">お問合せ・お見積</a>
-      </div>
-      <div class="header-right">
-        <p class="header-right-title">理念の文章が入ります</p>
-        <p>
-          母体が内装解体専門会社であるからこそ、一般的な施工管理よりも解体費用を抑える抑えることができます。つまり、
-          解体費用を抑えることで、余力のある資金が生まれ、使い道を広げることができるということです。
-          <br />選択のできる予算の使途も合わせてご提案します。
-        </p>
-      </div>
-    </div>
+    <div class="sp-menu"></div>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  setup() {
+    const links = reactive([
+      { text: "ホーム", to: "#" },
+      { text: "私達について", to: "#" },
+      { text: "ホーム", to: "#" },
+      { text: "ホーム", to: "#" },
+      { text: "ホーム", to: "#" },
+    ]);
+    return { links };
+  },
+};
 </script>
 
 <style scoped>
 header {
   padding: 0 0px;
-  margin-bottom: 80px;
+  height: 80px;
+}
+.menu {
+  display: flex;
+  gap: 40px;
+  margin-left: 40px;
+  align-items: center;
+  height: 100%;
+  @include tablet {
+    display: none;
+  }
 }
 
-.haeder-title {
-  font-family: DNPShueiShogoMinStd-Hv;
-  padding: 30 0px;
-  /* font-size: 84px; */
-}
-.header-main {
-  width: 100%;
-}
-.header-img {
-  background-image: url("~@/assets/img/グループ 120.png");
-}
 .menu a {
-  padding: 20px;
+  text-decoration: none;
+  color: #000;
 }
 .header-right {
   width: 50%;
@@ -65,9 +53,7 @@ header {
   font-size: 70px;
 }
 
-@media screen and (max-width: 1080px) {
-  .header-right {
-    width: 80%;
-  }
+.sp-menu {
+  position: fixed;
 }
 </style>
