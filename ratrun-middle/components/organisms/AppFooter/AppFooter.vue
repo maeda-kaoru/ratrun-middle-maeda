93047a6ef52fed92ec047384628fd49af664fb60
maeda kaoru <maedakaoru.0615@gmail.com>
2023-04-12 10:53:54 +0900

    4/12 footer仮完成

--- ratrun-middle/components/organisms/AppFooter/AppFooter.vue
+++ ratrun-middle/components/organisms/AppFooter/AppFooter.vue
@@ -1,31 +1,55 @@
 <template>
   <footer>
-    <div class="menu">
-      <nuxt-link v-for="(link, key) in links" :key="key" :to="link.to">{{
-        link.text
-      }}</nuxt-link>
-      <nuxt-link v-for="(link, key) in secondLinks" :key="key" :to="link.to">{{
-        link.text
-      }}</nuxt-link>
+    <div class="footer-area">
+      <div class="logo">
+        <img src="../../../assets/img/logo.svg" />
+        <img src="../../../assets/img/logo-text.svg" />
+      </div>
+      <div class="menu">
+        <div class="menu-left">
+          <nuxt-link v-for="(link, key) in links" :key="key" :to="link.to">{{
+            link.text
+          }}</nuxt-link>
+        </div>
+        <div class="menu-right">
+          <nuxt-link
+            v-for="(link, key) in secondlinks"
+            :key="key"
+            :to="link.to"
+            >{{ link.text }}</nuxt-link
+          >
+        </div>
+      </div>
+      <div class="footer-right">
+        <div class="address">
+          <p>
+            建築・建築デザイン・原状回復・内装解体・建物解体・アスベスト除去工事はウラシコにお任せください！
+          </p>
+          <p>
+            <b>株式会社ウラシコ<br class="sp-br" />CYPHER （サイファー）</b>
+            <!-- <div class="space"></div> -->
+            <br />〒454-0982
+            <br />愛知県名古屋市中川区西伏屋2丁目617番地
+            <!-- <div class="space"></div> -->
+            <br />TEL: 052-655-4146
+          </p>
+        </div>
+        <div class="icon">
+          <img src="../../../assets/img/icon-facebook.svg" />
+          <img src="../../../assets/img/icon-instagram.svg" />
+          <img src="../../../assets/img/icon-pintarest.svg" />
+        </div>
+      </div>
     </div>
-    <button class="img-area" @click="clickMenu">
-      <img src="../../../assets/img/hamburger.svg" />
-    </button>
-    <AppMobileMenu v-if="isMenu" />
+    <div class="Copyright">
+      <p>Copyright(c) 店舗設計・デザイン・施工 CYPHER. AllRights Reserved</p>
+    </div>
   </footer>
 </template>
 
 <script>
-import { AppMobileMenu } from "../../molecules/Menu";
-
 export default {
-  components: { AppMobileMenu },
-
   setup() {
-    const clickMenu = () => {
-      isMenu.value = !isMenu.value;
-    };
-    const isMenu = ref(false);
     const links = reactive([
       { text: "ホーム", to: "#" },
       { text: "私達について", to: "#" },
@@ -33,76 +57,173 @@ export default {
       { text: "ワークフロー", to: "#" },
       { text: "FAQ", to: "#" },
       { text: "事例紹介", to: "#" },
+    ]);
+    const secondlinks = reactive([
       { text: "会社概要", to: "#" },
       { text: "お問合わせ・お見積", to: "#" },
+      { text: "個人情報保護法について", to: "#" },
     ]);
-    const secondLinks = reactive([{ text: "個人情報保護法について", to: "#" }]);
-    return { links, clickMenu, isMenu, secondLinks };
+    return { links, secondlinks };
   },
 };
 </script>
 
 <style lang='scss' scoped>
-.img-area {
+footer {
+  padding: 0 0px;
+}
+.footer-area {
+  color: white;
   width: 100%;
-  display: none;
+  height: 450px;
+  background-color: #756d6d;
+  display: flex;
+  padding-top: 80px;
+  @include tablet {
+    height: 1200px;
+    flex-direction: column;
+    padding: 0px;
+  }
+}
+.logo {
+  width: 33%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+  @include tablet {
+    width: 100%;
+    height: 250px;
+    gap: 20px;
+    padding: 30px 0px 30px 0px;
+  }
+  img {
+    width: 230px;
+    @include tablet {
+      width: 180px;
+    }
+  }
+}
+.menu {
+  width: 33%;
+  height: 150px;
+  display: flex;
+  @include tablet {
+    width: 100%;
+    height: 400px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+}
 
+.menu-left {
+  width: 700px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+
+  @include tablet {
+    width: 60%;
+    height: 240px;
+  }
+}
+.menu-right {
+  width: 1000px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  @include tablet {
+    width: 60%;
+    height: 150px;
+  }
+}
+.footer-right {
+  width: 33%;
+  display: flex;
+  flex-direction: column;
+  @include tablet {
+    width: 100%;
+    height: 400px;
+    flex-direction: none;
+    align-items: center;
+    justify-content: center;
+  }
+}
+.address {
+  width: 400px;
+  height: 270px;
+  padding: 10px;
+  border: 1px solid;
+  outline: 1px solid;
+  outline-offset: 5px;
+  margin: 20px;
+
+  @include tablet {
+    width: 80%;
+    height: 350px;
+    padding: 20px;
+    outline: none;
+    outline-offset: none;
+    margin: 0px;
+  }
+  p {
+    line-height: 2em;
+  }
+}
+.sp-br {
+  display: none;
   @include tablet {
     display: block;
   }
 }
-img {
-  width: 15%;
-  display: flex;
-  margin-left: auto;
-  margin-right: 20px;
+
+.space {
+  display: none;
+  @include tablet {
+    display: block;
+  }
 }
-header {
-  padding: 0 0px;
-  height: 80px;
-}
-.menu {
+
+.icon {
+  width: 450px;
   display: flex;
-  gap: 40px;
-  margin-left: 40px;
-  align-items: center;
-  height: 100%;
+  justify-content: flex-end;
+  gap: 10px;
+  margin-top: 20px;
+  @include tablet {
+    display: none;
+  }
+  img {
+    width: 40px;
+  }
+}
+
+.img-area {
+  width: 100%;
   @include tablet {
     display: none;
   }
 }
 
-.menu a {
-  text-decoration: none;
-  color: #000;
-}
-.header-right {
-  width: 50%;
-  padding-right: 50px;
-  margin-left: auto;
-}
-.header-right-title {
-  font-size: 70px;
-}
-@include tablet {
-  .sp-menu {
-    position: fixed;
-    width: 100%;
-    height: 730px;
+.Copyright {
+  width: 100%;
+  height: 80px;
+  font-family: 小塚ゴシック Pr6N;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @include tablet{
+    // padding: 20px;
+    height: 150px;
   }
-  .sp-menu a {
-    text-decoration: none;
-    color: #000;
-  }
-  .mobile-menu {
-    width: 80%;
-    height: 650px;
-    text-align: left;
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
-    padding-left: 50px;
-    padding-top: 50px;
-  }
+
 }
+
 </style>
